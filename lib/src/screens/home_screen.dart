2b7d0bd0278123c1c924df617fe3ef0fe3c439b0
dheerajv09 <dheerajv634@gmail.com>
2022-03-20 12:41:45 +0530

    created logout screen

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:zoom_app/src/provider/bottom_navigation_controller.dart';
-import 'package:zoom_app/src/services/auth_service.dart';
+import 'package:zoom_app/src/screens/settings_screen.dart';
 import 'package:zoom_app/src/screens/history_meeting_screen.dart';
 import 'package:zoom_app/src/screens/meeting_screen.dart';
 import 'package:zoom_app/src/utils/colors.dart';
@@ -56,37 +56,6 @@ class HomeScreen extends StatelessWidget {
                 label: 'Settings',
               ),
             ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class SettingsScreen extends StatelessWidget {
-  const SettingsScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(18.0),
-      child: ElevatedButton(
-        onPressed: () => context.read<AuthService>().signOut(),
-        child: const Text(
-          'Log Out',
-          style: TextStyle(
-            fontSize: 17,
-          ),
-        ),
-        style: ElevatedButton.styleFrom(
-          primary: buttonColor,
-          minimumSize: const Size(
-            double.infinity,
-            50,
-          ),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30),
-            side: const BorderSide(color: buttonColor),
           ),
         ),
       ),
---
+++ lib/src/screens/settings_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:zoom_app/src/services/auth_service.dart';
+
+import '../utils/colors.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: ElevatedButton(
+        onPressed: () => context.read<AuthService>().signOut(),
+        child: const Text(
+          'Log Out',
+          style: TextStyle(
+            fontSize: 17,
+          ),
+        ),
+        style: ElevatedButton.styleFrom(
+          primary: buttonColor,
+          minimumSize: const Size(
+            double.infinity,
+            50,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+            side: const BorderSide(color: buttonColor),
+          ),
+        ),
+      ),
+    );
+  }
+}
