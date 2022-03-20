b89a8f496af0cc07877fbb70412b20a1ea95c56e
dheerajv09 <dheerajv634@gmail.com>
2022-03-20 12:37:08 +0530

    added google sign in button

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:zoom_app/src/services/auth_service.dart';
-import 'package:zoom_app/src/widgets/custom_button.dart';
+
+import '../utils/colors.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -14,7 +15,7 @@ class LoginScreen extends StatelessWidget {
         children: [
           const Text(
             'Start or join a meeting',
-            style:  TextStyle(
+            style: TextStyle(
               fontSize: 24,
               fontWeight: FontWeight.bold,
             ),
@@ -23,9 +24,29 @@ class LoginScreen extends StatelessWidget {
             padding: const EdgeInsets.symmetric(vertical: 38.0),
             child: Image.asset('assets/images/onboarding.jpg'),
           ),
-          CustomButton(
-            text: 'Google Sign In',
-            onPressed: () => context.read<AuthService>().signInWithGoogle(context),
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: ElevatedButton(
+              onPressed: () =>
+                  context.read<AuthService>().signInWithGoogle(context),
+              child: const Text(
+                'Google Sign In',
+                style: TextStyle(
+                  fontSize: 17,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: buttonColor,
+                minimumSize: const Size(
+                  double.infinity,
+                  50,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  side: const BorderSide(color: buttonColor),
+                ),
+              ),
+            ),
           ),
         ],
       ),
